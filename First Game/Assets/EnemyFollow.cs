04cda031450c0ac2125478675d81ff5f5ef147cb
GxSherman <69094903+GxSherman@users.noreply.github.com>
2020-08-08 22:22:49 -0300

    Primer Commit

---
+++ First Game/Assets/EnemyFollow.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour
+{
+
+    public GameObject player;
+    public float speed;
+
+    private Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        gameObject.GetComponent<Animator>().SetBool("siguiendo", true);
+        if (Vector2.Distance(transform.position, target.position) > 0.1f)
+        {
+
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+            
+        }
+
+       if (player.transform.position.x < transform.position.x)
+        {
+            gameObject.GetComponent<SpriteRenderer>().flipX = true;
+        } else
+        {
+            gameObject.GetComponent<SpriteRenderer>().flipX = false;
+        }
+       
+    }
+}
---
+++ First Game/Assets/PlayerController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{   
+    public int Maxspeed;
+    public int Maxspeedneg;
+    public int jumpforce;
+    bool canJump;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("left"))
+        {
+
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new UnityEngine.Vector2(-3000f * Time.deltaTime, 0));
+
+            gameObject.GetComponent<Animator>().SetBool("moving", true);
+            gameObject.GetComponent<SpriteRenderer>().flipX = true;
+        }
+
+       
+
+
+
+        if (Input.GetKey("right"))
+        {
+
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new UnityEngine.Vector2(3000f * Time.deltaTime, 0));
+            gameObject.GetComponent<Animator>().SetBool("moving", true);
+            gameObject.GetComponent<SpriteRenderer>().flipX = false;
+        }
+
+        if (!Input.GetKey("left") && !Input.GetKey("right"))
+        {
+
+            gameObject.GetComponent<Animator>().SetBool("moving", false);
+        }
+
+
+        if (Input.GetKey("up") && canJump)
+        {
+
+            canJump = false;
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new UnityEngine.Vector2(0, jumpforce));
+        }
+
+
+
+        gameObject.GetComponent<Rigidbody2D>().velocity = new UnityEngine.Vector2( Math.Min(gameObject.GetComponent<Rigidbody2D>().velocity.x, Maxspeed), gameObject.GetComponent<Rigidbody2D>().velocity.y);
+        gameObject.GetComponent<Rigidbody2D>().velocity = new UnityEngine.Vector2(Math.Max(gameObject.GetComponent<Rigidbody2D>().velocity.x, Maxspeedneg), gameObject.GetComponent<Rigidbody2D>().velocity.y);
+
+
+
+
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.transform.tag == "ground")
+
+            canJump = true;
+
+    }
+}
